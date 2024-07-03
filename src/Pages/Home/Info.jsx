ac19ce39c0ee85a22f72ba5739bf094bcc90e79f
Mohsin <mohsin@localhost.localdomain>
2024-07-03 22:52:53 +0600

    alighnment

--- src/Pages/Home/Info.jsx
+++ src/Pages/Home/Info.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Info = () => {
     return (
-        <div className='grid gap-5 px-20 lg:grid-cols-2 sm:grid-cols-1'>
+        <div className='grid gap-5 px-20 text-center lg:grid-cols-2 sm:grid-cols-1'>
             <div className="card card-compact bg- shadow-xl p-3 bg-[#2E2E2E]">
-                <div className="p-5 lg:flex lg:flex-row lg:justify-between lg:items-center block">
-                    <div className='text-white'>
+                <div className="block p-5 lg:flex lg:flex-row lg:justify-between lg:items-center">
+                    <div className='text-white '>
                         <p className='uppercase'>From, Bestelliers</p>
-                        <h2 className="card-title my-3 text-2xl">WORX Tools</h2>
+                        <h2 className="flex justify-center my-3 text-2xl card-title">WORX Tools</h2>
                         <button className="btn btn-sm bg-[#FBC42D] mt-4">Buy Now</button>
                     </div>
                     <div className='justify-end'>
@@ -16,11 +16,11 @@ const Info = () => {
                 </div>
             </div>
             <div className="card card-compact bg- shadow-xl p-3 bg-[#FE9116]">
-                <div className="p-5 lg:flex lg:flex-row lg:justify-between lg:items-center block">
+                <div className="block p-5 lg:flex lg:flex-row lg:justify-between lg:items-center">
                     <div className='text-white'>
                         <p className='uppercase'>From, Bestelliers</p>
-                        <h2 className="card-title my-3 text-2xl">WORX Tools</h2>
-                        <button className="btn btn-sm btn-neutral mt-4">Buy Now</button>
+                        <h2 className="flex justify-center my-3 text-2xl card-title">WORX Tools</h2>
+                        <button className="mt-4 btn btn-sm btn-neutral">Buy Now</button>
                     </div>
                     <div className='justify-end'>
                         <img style={{ width: 250 }} src="https://i.ibb.co/Hdgf9Fz/gun.png" alt="" />
